54ff17d45da7774b7998d3a412bcd6a04e689003
Qiming Xu <33349132+xqm32@users.noreply.github.com>
2025-11-17 00:27:12 +0800

    feat: override

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { YAML } from "bun";
+import override from "./override.yaml";
+
+const response = await fetch(process.env.SUB_URL!);
+const config = YAML.parse(await response.text()) as Record<string, unknown>;
+await Bun.write(
+  process.env.SUB_PATH ?? "/etc/mihomo/config.yaml",
+  YAML.stringify({ ...config, ...override }, null, 2)
+);
